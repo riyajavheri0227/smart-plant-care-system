bf28424c797fb73ef91224986b9de1e8ec3e242d
Riya Javheri <riyajavheri0227@gmail.com>
2026-08-03 21:50:09 +0530

    Added new feature

--- smartplantcare/urls.py
+++ smartplantcare/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+print("SMART PLANT CARE URLS LOADED")
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
--- website/admin.py
+++ website/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from .models import User, Plant, Contact, MyPlant
+from .models import (
+    User,
+    Plant,
+    Contact,
+    MyPlant,
+    AiScan,
+    WateringReminder,
+    ScheduleReminder,
+    FavoritePlant,
+    PushSubscription,
+)
+
+admin.site.site_header = "🌱 Smart Plant Care Admin"
+admin.site.site_title = "Smart Plant Care"
+admin.site.index_title = "Welcome to Smart Plant Care Admin"
 
 admin.site.register(User)
 admin.site.register(Plant)
 admin.site.register(Contact)
 admin.site.register(MyPlant)
-# Register your models here.
+admin.site.register(AiScan)
+admin.site.register(WateringReminder)
+admin.site.register(ScheduleReminder)
+admin.site.register(FavoritePlant)
+admin.site.register(PushSubscription)
